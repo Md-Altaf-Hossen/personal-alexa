6d503f7a87b3c8ca30261767f039270c07c08e50
md-altaf-hossen <mdaltafhossen2002@gmail.com>
2022-02-28 04:24:08 +0600

    photo added

--- js/main.js
+++ js/main.js
@@ -39,32 +39,33 @@ function read(text) {
     else if (text == "who are you") {
         speech.text = "i am assistant of Md Altaf Hossen"
     }
-    else if (text == "") {
-        speech.text = ""
+    else if (text == "who is your owner") {
+        speech.text = 'My boss is Md Altaf Hossen';
     }
-    else if (text == "") {
-        speech.text = ""
+    else if (text == "how old are you") {
+        speech.text = "i am computer programme , i have no age like human"
     }
-    else if (text == "") {
-        speech.text = ""
+    else if (text == "what food do you like") {
+        speech.text = "biriyani"
     }
-    else if (text == "") {
-        speech.text = ""
+    else if (text == "you are so talented") {
+        speech.text = "Thank you very much !!"
     }
-    else if (text == "") {
-        speech.text = ""
+    else if (text == "I love you") {
+        speech.text = "I love you too but as a friend !!"
     }
-    else if (text == "") {
-        speech.text = ""
+    else if (text == "why") {
+        speech.text = "nothing "
     }
-    else if (text == "") {
-        speech.text = ""
+    else if (text == "do you have any girlfriend") {
+        speech.text = "yes i have more and more girlfriends"
     }
-    else if (text == "") {
-        speech.text = ""
+    else if (text == "give me your boss phone number") {
+        speech.text = "+88 01873-379859"
     }
-    else if (text == "") {
-        speech.text = ""
+    else if (text == "give me your boss photo") {
+        speech.text = "";
+        document.getElementById('link').style.display = 'block';
     }
     else if (text == "") {
         speech.text = ""
